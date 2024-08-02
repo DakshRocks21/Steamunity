e442d45c4d3a56275282662434cbf17af010ddfe
DakshRocks21 <85815311+DakshRocks21@users.noreply.github.com>
2024-08-02 23:34:26 +0800

    V1: prototype working

--- doorbell/doorbell.ino
+++ doorbell/doorbell.ino
@@ -1,24 +1,26 @@
 #include <esp_now.h>
 #include <WiFi.h>
+#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);  // I2C address 0x27, 16 columns and 2 rows
+// Initialize the I2C LCD (address 0x27, 16 columns, 2 rows)
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-uint8_t broadcastAddress1[] = { 0xEC, 0x64, 0xC9, 0x98, 0x79, 0x5C };  // Replace with your actual receiver MAC Address
+uint8_t rangeExtenderAddress[] = {0xEC,0x64,0xc9,0x98,0x6e,0x70};  /* Range extender MAC address */ 
 
-const int buttonPin = 2; // 26 for Esp, 2 for Mini
+const int buttonPin = 26; // 26 for Esp, 2 for Mini
 bool buttonPressed = false;
 unsigned long lastSendTime = 0;
-const unsigned long sendTimeout = 15000;  // 15 seconds timeout
-const int toAccept = 21; // 25 for Esp, 21 for Mini (red LED)
-const int accepted = 20; // 33 for Esp, 20 for Mini (green LED)
-const int sdaPin = 6; // 23 for Esp, 6 for Mini
-const int sclPin = 7; // 19 for Esp, 7 for Mini
+const unsigned long sendTimeout = 2000;  // 15 seconds timeout
+const int toAccept = 25; // 25 for Esp, 21 for Mini (red LED)
+const int accepted = 33; // 33 for Esp, 20 for Mini (green LED)
+const int sdaPin = 23; // 23 for Esp, 6 for Mini
+const int sclPin = 19; // 19 for Esp, 7 for Mini`
 
 typedef struct struct_message {
   char message[32];
 } struct_message;
-xx
+
 struct_message myData;
 struct_message receivedData;
 
@@ -37,12 +39,18 @@ void OnDataRecv(const esp_now_recv_info *info, const uint8_t *incomingData, int 
   Serial.println(receivedData.message);
   Serial.println();
 
-  digitalWrite(accepted, HIGH);
-  digitalWrite(toAccept, LOW);
-
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Acknowledged!");
+  if (strcmp(receivedData.message, "ACK_1") == 0) {
+    digitalWrite(accepted, HIGH);
+    digitalWrite(toAccept, LOW);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Acknowledged!");
+  } else if (strcmp(receivedData.message, "FAILED") == 0) {
+    digitalWrite(toAccept, LOW);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Send Failed!");
+  }
 }
 
 void IRAM_ATTR handleButtonPress() {
@@ -50,12 +58,10 @@ void IRAM_ATTR handleButtonPress() {
 }
 
 void setup() {
-  // Initialize light pin
   pinMode(toAccept, OUTPUT);
   pinMode(accepted, OUTPUT);
 
-  // initialize LCD
-  Wire.begin(sdaPin, sclPin);  // SDA on GPIO 23, SCL on GPIO 19
+  Wire.begin(sdaPin, sclPin);
   lcd.init();
   lcd.backlight();
   lcd.clear();
@@ -64,34 +70,26 @@ void setup() {
   lcd.setCursor(0, 1);
   lcd.print("long to make");
 
-  // Init Serial Monitor
   Serial.begin(115200);
 
-  // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
 
-  // Init ESP-NOW
   if (esp_now_init() != ESP_OK) {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
 
-  // Register for Send CB to get the status of transmitted packet
   esp_now_register_send_cb(OnDataSent);
-
-  // Register for recv CB to get recv packet info
   esp_now_register_recv_cb(OnDataRecv);
 
-  // Register peer
-  memcpy(peerInfo.peer_addr, broadcastAddress1, 6);
+  memcpy(peerInfo.peer_addr, rangeExtenderAddress, 6);
   peerInfo.channel = 0;
   peerInfo.encrypt = false;
   if (esp_now_add_peer(&peerInfo) != ESP_OK) {
-    Serial.println("Failed to add peer 1");
+    Serial.println("Failed to add peer");
     return;
   }
 
-  // Initialize button pin as input
   pinMode(buttonPin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(buttonPin), handleButtonPress, FALLING);
 }
@@ -105,14 +103,12 @@ void loop() {
     if (currentTime - lastSendTime >= sendTimeout) {
       lastSendTime = currentTime;
 
-      // Set message to send
       strcpy(myData.message, "LIGHT ON");
 
-      // Send message via ESP-NOW to both peers
-      esp_err_t result1 = esp_now_send(broadcastAddress1, (uint8_t *)&myData, sizeof(myData));
+      esp_err_t result = esp_now_send(rangeExtenderAddress, (uint8_t *)&myData, sizeof(myData));
 
-      if (result1 == ESP_OK) {
-        Serial.println("Sent with success to peers");
+      if (result == ESP_OK) {
+        Serial.println("Sent with success to range extender");
         lcd.clear();
         lcd.setCursor(0, 0);
         lcd.print("Calling!");
@@ -120,6 +116,10 @@ void loop() {
         lcd.print("Wait a bit...");
       } else {
         Serial.println("Error sending the data");
+        digitalWrite(toAccept, LOW);
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Send Failed!");
       }
     } else {
       Serial.println("Timeout in effect. Please wait before sending again.");
--- watch/watch.ino
+++ watch/watch.ino
@@ -2,23 +2,15 @@
 #include <WiFi.h>
 #include "pitches.h"
 
-// REPLACE WITH YOUR RECEIVER MAC Address
-uint8_t broadcastAddress[] = { 0xEC, 0x64, 0xC9, 0x98, 0x7E, 0x10 };  // Daksh' Board
-// uint8_t broadcastAddress[] = {0x08, 0xF9, 0xE0, 0xF6, 0xE9, 0xE4}; // NOT Daksh' Board, WC Board
-//uint8_t broadcastAddress[] = {0xEC, 0x64, 0xC9, 0x98, 0x79, 0x5C}; // NOT Daksh' Board, Richards Board
+uint8_t rangeExtenderAddress[] = {0xEC,0x64,0xc9,0x98,0x6e,0x70}; /* Range extender MAC address */ 
 
 int melody[] = {
-  // NOTE_C4, 0, NOTE_G4, 0, NOTE_AS4, NOTE_C5, NOTE_AS4, 0, NOTE_F4, NOTE_DS4, 0,
-  // NOTE_C4, 0, NOTE_G4, 0, NOTE_AS4, NOTE_C5, NOTE_AS4, 0, NOTE_F4, NOTE_DS4, 0,
-  // NOTE_C4, 0, NOTE_G4, 0, NOTE_AS4, NOTE_C5, NOTE_AS4, 0, NOTE_F4, NOTE_DS4, 0,
-
   NOTE_C4, 0, NOTE_E4, 0, NOTE_G4, NOTE_A4, NOTE_AS4,
   NOTE_C5, 0, NOTE_C5, 0, NOTE_AS4, 0, NOTE_A4, 0,
   NOTE_AS4, 0, NOTE_AS4, NOTE_C5, 0, NOTE_AS4, NOTE_A4, 0,
   0,
   NOTE_C5, 0, NOTE_AS4, 0, NOTE_A4, 0, NOTE_AS4, 0, NOTE_E5,
   0,
-
   NOTE_C5, 0, NOTE_C5, 0, NOTE_AS4, 0, NOTE_A4, 0,
   NOTE_AS4, 0, NOTE_AS4, NOTE_C5, 0, NOTE_AS4, NOTE_A4, 0,
   0,
@@ -26,57 +18,38 @@ int melody[] = {
 };
 
 int noteDurations[] = {
-  // 4, 8, 4, 8, 4, 8, 8, 16, 8, 8, 16,
-  // 4, 8, 4, 8, 4, 8, 8, 16, 8, 8, 16,
-  // 4, 8, 4, 8, 4, 8, 8, 16, 8, 8, 16,
-
   4, 8, 4, 8, 4, 4, 4,
   8, 16, 8, 16, 8, 16, 8, 16,
   8, 16, 8, 8, 16, 8, 8, 16,
   4,
   8, 16, 8, 16, 8, 16, 8, 4, 8,
   4,
-
   8, 16, 8, 16, 8, 16, 8, 16,
   8, 16, 8, 8, 16, 8, 8, 16,
   4,
   8, 16, 8, 16, 8, 16, 8, 4, 8, 2
 };
 
-// ESP_TINY
-// Tested on a big board - to be modified
-// Button - 12
-// LED - 26
-// Buzzer 25
-
-// Defining Pins
 const int buttonPin = 12;
 const int ledPin = 26;
 const int buzzerPin = 25;
 bool buttonPressed = false;
 bool lightOnReceived = false;
 
-// Structure example to send data
-// Must match the receiver structure
 typedef struct struct_message {
   char message[32];
 } struct_message;
 
-// Create a struct_message called myData
 struct_message myData;
-
-// Create a struct_message to receive data
 struct_message receivedData;
 
 esp_now_peer_info_t peerInfo;
 
-// callback when data is sent
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   Serial.print("\r\nLast Packet Send Status:\t");
   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
 
-// callback function that will be executed when data is received
 void OnDataRecv(const esp_now_recv_info *info, const uint8_t *incomingData, int len) {
   memcpy(&receivedData, incomingData, sizeof(receivedData));
   Serial.print("Bytes received: ");
@@ -87,8 +60,7 @@ void OnDataRecv(const esp_now_recv_info *info, const uint8_t *incomingData, int 
 
   if (strcmp(receivedData.message, "LIGHT ON") == 0) {
     lightOnReceived = true;
-    // digitalWrite(25, HIGH); // Turn on buzzer
-    digitalWrite(ledPin, HIGH);  // Turn on light
+    digitalWrite(ledPin, HIGH);
   }
 }
 
@@ -97,40 +69,30 @@ void IRAM_ATTR handleButtonPress() {
 }
 
 void setup() {
-  // lightPin and buzzerPin
-  pinMode(buzzerPin, OUTPUT);  // Buzzer
-  pinMode(ledPin, OUTPUT);     // Light
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
 
-  // Init Serial Monitor
   Serial.begin(115200);
 
-  // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
 
-  // Init ESP-NOW
   if (esp_now_init() != ESP_OK) {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
 
-  // Register for Send CB to get the status of transmitted packet
   esp_now_register_send_cb(OnDataSent);
-
-  // Register for recv CB to get recv packet info
   esp_now_register_recv_cb(OnDataRecv);
 
-  // Register peer
-  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  memcpy(peerInfo.peer_addr, rangeExtenderAddress, 6);
   peerInfo.channel = 0;
   peerInfo.encrypt = false;
 
-  // Add peer
   if (esp_now_add_peer(&peerInfo) != ESP_OK) {
     Serial.println("Failed to add peer");
     return;
   }
 
-  // Initialize button pin as input
   pinMode(buttonPin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(buttonPin), handleButtonPress, FALLING);
 }
@@ -140,11 +102,9 @@ void loop() {
     buttonPressed = false;
     lightOnReceived = false;
 
-    // Set response message
     strcpy(myData.message, "ACK");
 
-    // Send response message via ESP-NOW
-    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));
+    esp_err_t result = esp_now_send(rangeExtenderAddress, (uint8_t *)&myData, sizeof(myData));
 
     if (result == ESP_OK) {
       Serial.println("ACK sent with success");
@@ -161,11 +121,10 @@ void loop() {
       delay(pauseBetweenNotes);
       noTone(buzzerPin);
       if (buttonPressed) {
-        digitalWrite(ledPin, LOW);  // Turn off light
-        noTone(buzzerPin);          // Turn off buzzer
+        digitalWrite(ledPin, LOW);
+        noTone(buzzerPin);
         break;
       }
     }
   }
-  // delay(500);
 }
